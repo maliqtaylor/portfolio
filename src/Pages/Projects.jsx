c89c5ee744d690bea7ef9da6f8f567d06719d292
maliqtaylor <maliq.taylor@icloud.com>
2021-03-19 16:05:32 -0400

    add project images

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Footer from '../Components/Footer'
 import Card from 'react-bootstrap/Card'
+import { Image } from 'semantic-ui-react'
+import tetris from '../images/tetris.png'
+import watchlist from '../images/watchlist.png'
+import flock from '../images/flock.png'
+
 
 
 function Projects() {
@@ -8,9 +13,39 @@ function Projects() {
     <div className='projects'>
       <div id='page-wrap'>
 
-        <Card className='projectCard'>
+        <Card className='projectsCard'>
           <Card.Body>
-          <Card.Title className='cardTitle'>Projects</Card.Title>
+            <Card className='project' border='primary'>
+              <Card.Title className='cardTitle'>Tetris</Card.Title>
+              <Image
+                src={tetris}
+                size='huge'
+                verticalAlign='middle'
+                bordered={true}
+              />
+            </Card>
+
+            <Card className='project' border='primary'>
+              <Card.Title className='cardTitle'>Anime Watchlist</Card.Title>
+              <Image
+                src={watchlist}
+                size='huge'
+                verticalAlign='middle'
+                bordered={true}
+              />
+            </Card>
+
+            <Card className='project' border='primary'>
+              <Card.Title className='cardTitle'>Wryters Flock</Card.Title>
+              <Image
+                src={flock}
+                size='huge'
+                verticalAlign='middle'
+                bordered={true}
+              />
+            </Card>
+
+
           </Card.Body>
         </Card>
 
