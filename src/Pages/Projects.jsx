b4fde6307f6998035ea4fd0b263e0349a525e1a2
maliqtaylor <maliq.taylor@icloud.com>
2021-03-19 01:57:33 -0400

    add projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Footer from '../Components/Footer'
+import Card from 'react-bootstrap/Card'
 
 
 function Projects() {
   return (
-    <div className='about'>
+    <div className='projects'>
       <div id='page-wrap'>
+
+        <Card className='projectCard'>
+          <Card.Body>
+          <Card.Title className='cardTitle'>Projects</Card.Title>
+          </Card.Body>
+        </Card>
 
         <Footer left='/about' right='/socials' />
       </div>
