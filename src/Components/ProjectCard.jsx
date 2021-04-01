9333598df22e9dae3195707b65c2181adcd87d5b
maliqtaylor <maliq.taylor@icloud.com>
2021-03-31 21:40:31 -0400

    create project component

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react'
+import Card from 'react-bootstrap/Card'
+import tetris from '../images/tetris.png'
+import watchlist from '../images/watchlist.png'
+import flock from '../images/flock.png'
+
+const projects = [
+  {
+    title: 'Tetris',
+    img: tetris,
+    github: "https://github.com/maliqtaylor/tetris-with-dom",
+    live: "https://tetris-dom-project.surge.sh/",
+    desc: 'A recreation of Tetris using JavaScript CSS and HTML DOM manipulation.'
+  },
+  {
+    title: 'Anime Watchlist',
+    img: watchlist,
+    github: "https://github.com/maliqtaylor/anime-watchlist",
+    live: "https://anime-watchlist.herokuapp.com/",
+    desc: 'A full-stack web application designed for users to catalogue their favorite Anime shows and movies.'
+  },
+  {
+    title: 'Wryters Flock',
+    img: flock,
+    github: "https://github.com/maliqtaylor/wryters-flock",
+    live: "https://wryters-flock.herokuapp.com/",
+    desc: 'A full-stack React web application built for writers to post creative writing pieces and get feedback.'
+  }
+]
+
+
+function ProjectCard() {
+  return (
+    <>
+      {
+        projects.map((proj, i) =>
+          <Card className='project' border='primary' key={i}>
+            <Card.Title className='cardTitle'>
+              {proj.title}
+              <br />
+
+              <a
+                className='pointer hub'
+                rel='noreferrer'
+                target="_blank"
+                href={proj.github}
+                style={{ fontSize: 'medium' }}
+              >
+                Github
+              </a>
+
+              <a
+                className='pointer live'
+                rel='noreferrer'
+                target="_blank"
+                href={proj.live}
+                style={{ fontSize: 'medium' }}
+              >
+                Live
+              </a>
+
+            </Card.Title>
+
+            <Image
+              src={proj.img}
+              size='huge'
+              verticalAlign='middle'
+              bordered={true}
+            />
+
+            <Card.Body>
+              {proj.desc}
+            </Card.Body>
+          </Card>
+        )
+      }
+    </>
+  );
+}
+
+export default ProjectCard;
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import Footer from '../Components/Footer'
+import ProjectCard from '../Components/ProjectCard'
 import Card from 'react-bootstrap/Card'
-import { Image, Icon } from 'semantic-ui-react'
-import tetris from '../images/tetris.png'
-import watchlist from '../images/watchlist.png'
-import flock from '../images/flock.png'
-
-
-
 
 function Projects() {
   return (
@@ -18,103 +12,7 @@ function Projects() {
           <h1>Projects</h1>
 
           <Card.Body>
-            <Card className='project' border='primary'>
-              <Card.Title className='cardTitle'>
-                <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://tetris-dom-project.surge.sh/"
-                >
-                  Tetris
-              </a> |
-
-               <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://github.com/maliqtaylor/tetris-with-dom"
-                >
-                  {' '}<Icon name='git' size='small' />
-                </a>
-
-              </Card.Title>
-              <Image
-                src={tetris}
-                size='huge'
-                verticalAlign='middle'
-                bordered={true}
-              />
-              <Card.Body>
-                A recreation of Tetris using JavaScript CSS and HTML DOM manipulation.
-              </Card.Body>
-            </Card>
-
-            <Card className='project' border='primary'>
-              <Card.Title className='cardTitle'>
-                <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://anime-watchlist.herokuapp.com/"
-                >
-                  Anime Watchlist
-              </a> |
-                <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://github.com/maliqtaylor/anime-watchlist"
-                >
-                  {' '}<Icon name='git' size='small' />
-                </a>
-              </Card.Title>
-
-              <Image
-                src={watchlist}
-                size='huge'
-                verticalAlign='middle'
-                bordered={true}
-              />
-              <Card.Body>
-                A full-stack web application designed for users to catalogue their favorite Anime shows and movies.
-              </Card.Body>
-            </Card>
-
-            <Card className='project' border='primary'>
-              <Card.Title className='cardTitle'>
-                <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://wryters-flock.herokuapp.com/"
-                >
-                  Wryters Flock
-              </a> |
-
-                 <a
-                  className='pointer'
-                  rel='noreferrer'
-                  target="_blank"
-                  href="https://github.com/maliqtaylor/wryters-flock"
-                >
-                  {' '}<Icon name='git' size='small' />
-                </a>
-              </Card.Title>
-
-
-              <Image
-                src={flock}
-                size='huge'
-                verticalAlign='middle'
-                bordered={true}
-              />
-              <Card.Body>
-                A full-stack React web application built for writers to post creative writing pieces and get feedback.
-              </Card.Body>
-            </Card>
-
-
+            <ProjectCard />
           </Card.Body>
         </Card>
 
