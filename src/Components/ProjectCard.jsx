c0a176245e06d8b15bbc6a02bc38720883277286
maliqtaylor <maliq.taylor@icloud.com>
2021-04-05 23:45:07 -0400

    change css dimensions

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -35,44 +35,47 @@ function ProjectCard() {
     <Container>
       {
         projects.map((proj, i) =>
-          <Card className='project' border='primary' key={i}>
-            <Card.Title className='cardTitle'>
-              {proj.title}
-              <br />
+          <Container>
+            <Card className='project' border='primary' key={i}>
+              <Card.Title className='cardTitle'>
+                {proj.title}
+                <br />
 
-              <a
-                className='pointer hub'
-                rel='noreferrer'
-                target="_blank"
-                href={proj.github}
-                style={{ fontSize: 'medium' }}
-              >
-                Github
+                <a
+                  className='pointer hub'
+                  rel='noreferrer'
+                  target="_blank"
+                  href={proj.github}
+                  style={{ fontSize: 'medium' }}
+                >
+                  Github
               </a>
                 |
               <a
-                className='pointer live'
-                rel='noreferrer'
-                target="_blank"
-                href={proj.live}
-                style={{ fontSize: 'medium' }}
-              >
-                Live
+                  className='pointer live'
+                  rel='noreferrer'
+                  target="_blank"
+                  href={proj.live}
+                  style={{ fontSize: 'medium' }}
+                >
+                  Live
               </a>
 
-            </Card.Title>
+              </Card.Title>
 
-            <Image
-              src={proj.img}
-              size='huge'
-              verticalAlign='middle'
-              bordered={true}
-            />
+              <Card.Body>
+                <Image
+                  src={proj.img}
+                  size='huge'
+                  verticalAlign='middle'
+                  bordered={true}
+                />
+                <br/>
 
-            <Card.Body>
-              {proj.desc}
-            </Card.Body>
-          </Card>
+                {proj.desc}
+              </Card.Body>
+            </Card>
+          </Container>
         )
       }
     </Container>
