407f819cfb42de8c6bc3390c0f7afbf46daf8063
maliqtaylor <maliq.taylor@icloud.com>
2021-04-07 12:18:33 -0400

    update about me

--- src/Components/AboutCard.jsx
+++ src/Components/AboutCard.jsx
@@ -18,12 +18,12 @@ function AboutCard() {
         />
 
         <Card.Text className='text'>
-          Hello and welcome to my portfolio. I'm Maliq Taylor; a full-stack web developer from Brooklyn, New York. My tech journey started in 2019 when I became a <em>Full Stack Web Development</em> fellow at <a rel='noreferrer' target="_blank" href='https://www.pursuit.org/'>Pursuit</a>. In 2020 I continued my engineering education and joined <a rel='noreferrer' target="_blank" href='https://generalassemb.ly/about'>General Assembly</a> as a <em>Software Engineering Immersive</em> student. My time at  Pursuit & General Assembly was spent mastering web technoligies like Javascript, Python, React, HTML, SQL, etc. In addition to improving my technical skills, I was also able to express my creativity through code and work with a wide variety of people. The opportunity to collaborate & learn from others is probably my favorite aspect of being a developer.
+          Hello and welcome to my portfolio. I'm Maliq Taylor; a full-stack web developer from Brooklyn, New York. My tech journey started in 2019 when I became a <em>Full Stack Web Development</em> fellow at <a rel='noreferrer' target="_blank" href='https://www.pursuit.org/'>Pursuit</a>. In 2020 I continued my engineering education and joined <a rel='noreferrer' target="_blank" href='https://generalassemb.ly/about'>General Assembly</a> as a <em>Software Engineering Immersive</em> student. My time at Pursuit & General Assembly was spent mastering web technologies like JavaScript, Python, React, HTML, SQL, etc. In addition to improving my technical skills, I was also able to express my creativity through code and work with a wide variety of people. The opportunity to collaborate & learn from others is probably my favorite aspect of being a developer.
 
           <br />
           <br />
 
-          Beyond technology, my hobbies & interests include literature, astronomy, travel and sports. Reading and writing are distinct passions of mine. There's nothing quite like taking a deep dive into a thoughtfully crafted naritive and I aspire to one day be a published author and entertain others with my writing & imagination.
+          Beyond technology, my hobbies & interests include literature, astronomy, travel and sports. Reading and writing are distinct passions of mine. There's nothing quite like taking a deep dive into a thoughtfully crafted narrative and I aspire to one day be a published author and entertain others with my writing & imagination.
         </Card.Text>
       </Card.Body>
 
